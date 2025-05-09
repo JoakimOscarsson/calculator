4727867547c9d0eb7e4083d0eb1215b3f64f9d75
Joakim Oscarsson <8088995+JoakimOscarsson@users.noreply.github.com>
2025-05-09 17:02:52 +0200

    Now supports VM execution for +,-,*,/,parentheses and unarys

--- interpreter.py
+++ interpreter.py
@@ -1,12 +1,6 @@
 from dataclasses import dataclass
-from enum import Enum, auto
+from enum import IntEnum, Enum, auto
 from typing import Protocol, Any
-# read input
-# Tokenize
-# syntax parsing
-# Semantic-analysis
-# Evaluation
-# print
 
 class TokenType(Enum):
     ADD = " + "
@@ -20,25 +14,13 @@ class TokenType(Enum):
     UNARY_PLUS = " +"
     UNARY_MINUS = " -"
     EOF = "EOF"
-    @property
-    def operation(self):
-        return {
-            TokenType.ADD: lambda l, r: l + r, 
-            TokenType.SUBTRACT: lambda l,r: l - r,
-            TokenType.MULTIPLY: lambda l,r: l * r,
-            TokenType.DIVIDE: lambda l,r: l / r,
-            TokenType.POWER: lambda l,r: l**r,
-            TokenType.UNARY_PLUS: lambda o: o,
-            TokenType.UNARY_MINUS: lambda o: -o
-        }.get(self, None)
+
 
 @dataclass
 class Token:
     token_type: TokenType
     value: Any = None
-    @property
-    def operation(self):
-        return self.token_type.operation
+
 
 class PeakIter:
     def __init__(self, iterable):
@@ -65,57 +47,58 @@ class PeakIter:
     def peak(self):
         self.buffert = self.next()
         return self.buffert
-        
 
-def tokenize(str):
+
+class Lexer:
     def build_number(number: str, iter: PeakIter):
         try:
             peaked = iter.peak()
         except StopIteration:
             return number
         if peaked in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."]:
-            return build_number(number+iter.next(), iter)
+            return Lexer.build_number(number+iter.next(), iter)
         else:
             return number
-    
-    tokens = []
-    iter = PeakIter(str)
-    while True:
-        try:
-            char = iter.next()
-            match char: 
-                case "+":
-                    tokens.append(Token(TokenType.ADD))
-                case "-":
-                    tokens.append(Token(TokenType.SUBTRACT))
-                case "*":
-                    if iter.peak() == "*":
-                        iter.next()
-                        tokens.append(Token(TokenType.POWER))
-                    else:
-                        tokens.append(Token(TokenType.MULTIPLY))
-                case "/":
-                    tokens.append(Token(TokenType.DIVIDE))
-                case "!":
-                    raise SyntaxError("! not yet implemented")  # Todo!
-                case "(":
-                    tokens.append(Token(TokenType.OPEN_PARENTHESES))
-                case ")":
-                    tokens.append(Token(TokenType.CLOSE_PARENTHESES))
-                case " ":
-                    pass
-                case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9":
-                    tokens.append(
-                        Token(
-                            TokenType.NUMBER, float(build_number(char, iter))
+
+    def tokenize(str):
+        tokens = []
+        iter = PeakIter(str)
+        while True:
+            try:
+                char = iter.next()
+                match char: 
+                    case "+":
+                        tokens.append(Token(TokenType.ADD))
+                    case "-":
+                        tokens.append(Token(TokenType.SUBTRACT))
+                    case "*":
+                        if iter.peak() == "*":
+                            iter.next()
+                            tokens.append(Token(TokenType.POWER))
+                        else:
+                            tokens.append(Token(TokenType.MULTIPLY))
+                    case "/":
+                        tokens.append(Token(TokenType.DIVIDE))
+                    case "!":
+                        raise SyntaxError("! not yet implemented")  # Todo!
+                    case "(":
+                        tokens.append(Token(TokenType.OPEN_PARENTHESES))
+                    case ")":
+                        tokens.append(Token(TokenType.CLOSE_PARENTHESES))
+                    case " ":
+                        pass
+                    case "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9":
+                        tokens.append(
+                            Token(
+                                TokenType.NUMBER, float(Lexer.build_number(char, iter))
+                            )
                         )
-                    )
-                case _:
-                    raise SyntaxError(f"Invalid Expression {char}")
-        except StopIteration:
-            break
-    tokens.append(Token(TokenType.EOF))
-    return tokens
+                    case _:
+                        raise SyntaxError(f"Invalid Expression {char}")
+            except StopIteration:
+                break
+        tokens.append(Token(TokenType.EOF))
+        return tokens
         
 
 class Expression(Protocol):
@@ -138,6 +121,7 @@ class BinaryOperator():
     def to_str(self) -> str:
         return f"({self.left.to_str()} {self.operator.token_type.value} {self.right.to_str()})"
 
+
 @dataclass
 class UnaryOperator():
     operator: Token
@@ -146,92 +130,236 @@ class UnaryOperator():
         return f"{self.operator.token_type.value}{self.operand.to_str()}"
 
 
-def parse(tokens: [Token]) -> Expression:
-    ast = parse_additive(tokens)
-    if tokens[0].token_type != TokenType.EOF:
-        raise SyntaxError("Error, expected end of file!")
-    return ast
+class Parser:
+    def parse(tokens: [Token]) -> Expression:
+        ast = Parser.parse_additive(tokens)
+        if tokens[0].token_type != TokenType.EOF:
+            raise SyntaxError("Error, expected end of file!")
+        return ast
 
-def parse_unary(tokens: [Token]) -> Expression:
-    match tokens.pop(0).token_type:
-        case TokenType.ADD:
-            operator = Token(TokenType.UNARY_PLUS)
-        case TokenType.SUBTRACT:
-            operator = Token(TokenType.UNARY_MINUS)
-        case _:
-            raise SyntaxError("Unknown Unary operator")
-            
-    operand = parse_power(tokens)
-    return UnaryOperator(operator, operand)
+    def parse_unary(tokens: [Token]) -> Expression:
+        match tokens.pop(0).token_type:
+            case TokenType.ADD:
+                operator = Token(TokenType.UNARY_PLUS)
+            case TokenType.SUBTRACT:
+                operator = Token(TokenType.UNARY_MINUS)
+            case _:
+                raise SyntaxError("Unknown Unary operator")    
+        operand = Parser.parse_power(tokens)
+        return UnaryOperator(operator, operand)
     
+    def parse_parenthesised(tokens: [Token]) -> Expression:
+        tokens.pop(0)
+        expr = Parser.parse_additive(tokens)
+        if tokens[0].token_type != TokenType.CLOSE_PARENTHESES:
+            raise SyntaxError("Error, parentheses not closed")
+        tokens.pop(0)
+        return expr
 
-def parse_parenthesised(tokens: [Token]) -> Expression:
-    tokens.pop(0)
-    expr = parse_additive(tokens)
-    if tokens[0].token_type != TokenType.CLOSE_PARENTHESES:
-        raise SyntaxError("Error, parentheses not closed")
-    tokens.pop(0)
-    return expr
-
-
-def parse_additive(tokens: [Token]) -> Expression:
-    left = parse_multiplicative(tokens) 
-    while tokens[0].token_type == TokenType.ADD or tokens[0].token_type == TokenType.SUBTRACT:
-        operator = tokens.pop(0)
-        right = parse_multiplicative(tokens)
-        left = BinaryOperator(left, operator, right)
-    return left
-    
-
-def parse_multiplicative(tokens: [Token]) -> Expression:
-    left = parse_power(tokens)
-    while tokens[0].token_type in [TokenType.MULTIPLY, TokenType.DIVIDE, TokenType.OPEN_PARENTHESES]:
-        if tokens[0].token_type ==  TokenType.OPEN_PARENTHESES:
-            operator = Token(TokenType.MULTIPLY)
-            right = parse_parenthesised(tokens)
-        else:
+    def parse_additive(tokens: [Token]) -> Expression:
+        left = Parser.parse_multiplicative(tokens) 
+        while tokens[0].token_type == TokenType.ADD or tokens[0].token_type == TokenType.SUBTRACT:
             operator = tokens.pop(0)
-            right = parse_power(tokens)
-            if operator.token_type is TokenType.DIVIDE and right == 0:
-                raise ZeroDivisionError()
-        left = BinaryOperator(left, operator, right)
-    return left
+            right = Parser.parse_multiplicative(tokens)
+            left = BinaryOperator(left, operator, right)
+        return left
+
+    def parse_multiplicative(tokens: [Token]) -> Expression:
+        left = Parser.parse_power(tokens)
+        while tokens[0].token_type in [TokenType.MULTIPLY, TokenType.DIVIDE, TokenType.OPEN_PARENTHESES]:
+            if tokens[0].token_type ==  TokenType.OPEN_PARENTHESES:
+                operator = Token(TokenType.MULTIPLY)
+                right = Parser.parse_parenthesised(tokens)
+            else:
+                operator = tokens.pop(0)
+                right = Parser.parse_power(tokens)
+                if operator.token_type is TokenType.DIVIDE and right == 0:
+                    raise ZeroDivisionError()
+            left = BinaryOperator(left, operator, right)
+        return left
+
+    def parse_power(tokens: [Token]) -> Expression:
+        left = Parser.parse_primitive(tokens) 
+        while tokens[0].token_type == TokenType.POWER:
+            operator = tokens.pop(0)
+            right = Parser.parse_power(tokens)
+            left = BinaryOperator(left, operator, right)
+        return left
+
+    def parse_primitive(tokens: [Token]) -> Expression:
+        match tokens[0].token_type:
+            case TokenType.NUMBER:
+                return Literal(tokens.pop(0))
+            case TokenType.OPEN_PARENTHESES:
+                return Parser.parse_parenthesised(tokens)
+            case TokenType.ADD | TokenType.SUBTRACT:
+                return Parser.parse_unary(tokens)
+            case _:
+                raise SyntaxError(f"Unexpected token when parsing primitive {tokens[0].token_type}")
 
 
-def parse_power(tokens: [Token]) -> Expression:
-    left = parse_primitive(tokens) 
-    while tokens[0].token_type == TokenType.POWER:
-        operator = tokens.pop(0)
-        right = parse_primitive(tokens)
-        left = BinaryOperator(left, operator, right)
-    return left
+class Evaluator():
+    OPERATIONS = {
+        TokenType.ADD: lambda l, r: l + r, 
+        TokenType.SUBTRACT: lambda l,r: l - r,
+        TokenType.MULTIPLY: lambda l,r: l * r,
+        TokenType.DIVIDE: lambda l,r: l / r,
+        TokenType.POWER: lambda l,r: l**r,
+        TokenType.UNARY_PLUS: lambda o: o,
+        TokenType.UNARY_MINUS: lambda o: -o
+    }
+
+    def evaluate(ast: Expression):
+        if type(ast) is Literal:
+            return ast.token.value
+        elif type(ast) is BinaryOperator:
+            return Evaluator.OPERATIONS[ast.operator.token_type](Evaluator.evaluate(ast.left), Evaluator.evaluate(ast.right))
+        elif type(ast) is UnaryOperator:
+            return Evaluator.OPERATIONS[ast.operator.token_type](Evaluator.evaluate(ast.operand))
+        else:
+            raise SyntaxError("Unable to evaluate")
 
 
-def parse_primitive(tokens: [Token]) -> Expression:
-    match tokens[0].token_type:
-        case TokenType.NUMBER:
-            return Literal(tokens.pop(0))
-        case TokenType.OPEN_PARENTHESES:
-            return parse_parenthesised(tokens)
-        case TokenType.ADD | TokenType.SUBTRACT:
-            return parse_unary(tokens)
-        case _:
-            raise SyntaxError(f"Unexpected token when parsing primitive {tokens[0].token_type}")
+class Compiler:
+    def __init__(self):
+        self.OPERATIONS = {
+            TokenType.ADD: lambda l, r: l + r + [Op.ADD],
+            TokenType.SUBTRACT: lambda l, r: l + r + [Op.SUB],
+            TokenType.MULTIPLY: lambda l, r: l +  r + [Op.MUL],
+            TokenType.DIVIDE: lambda l, r: l + r + [Op.DIV],
+            TokenType.UNARY_PLUS: lambda o: o,
+            TokenType.UNARY_MINUS: lambda o: [Op.PUSH] + [-1] + o + [Op.MUL],
+        }
+    def compile(self, ast:Expression):
+        return self._compile(ast)+[Op.PRINT, Op.HALT]
 
+    def _compile(self, ast: Expression):
+        if type(ast) is Literal:
+            return [Op.PUSH, ast.token.value]
+        elif type(ast) is BinaryOperator:
+            return self.OPERATIONS.get(ast.operator.token_type)(self._compile(ast.left), self._compile(ast.right))
+        elif type(ast) is UnaryOperator:
+            return  self.OPERATIONS.get(ast.operator.token_type)(self._compile(ast.operand))
+        else:
+            raise SyntaxError("Error - Unknown expression")
 
-def evaluator(ast: Expression):
-    if type(ast) is Literal:
-        return ast.token.value
-    elif type(ast) is BinaryOperator:
-        return ast.operator.operation(evaluator(ast.left), evaluator(ast.right))
-    elif type(ast) is UnaryOperator:
-        return ast.operator.operation(evaluator(ast.operand))
-    else:
-        raise SyntaxError("Unable to evaluate")
+# Opcodes:
+class Op(IntEnum):
+    HALT = 0x01
+    PUSH = 0x02
+    PRINT = 0x03
+    ADD = 0x04
+    SUB = 0x05
+    MUL = 0x06
+    DIV = 0x07
 
+class Vm:
+    IMMEDIATES = {
+        Op.HALT: 0,
+        Op.PUSH: 1,
+        Op.PRINT: 0,
+        Op.ADD: 0,
+        Op.SUB: 0,
+        Op.MUL: 0,
+        Op.DIV: 0,
+    }
+
+    def print_program(self):
+        self.ip = 0
+        while self.ip < self.program_length:
+            instruction = Op(self.memory[self.ip])
+            n_immediates = Vm.IMMEDIATES[instruction]
+            immediates = self.memory[self.ip+1:self.ip+1+n_immediates]
+            print(instruction.name, *immediates)
+            self.ip += (1+n_immediates)
+
+    def __init__(self, memory_size=1024):
+        self.dispatcher = {
+            Op.HALT: self.exec_halt,
+            Op.PUSH: self.exec_push,
+            Op.PRINT: self.exec_print,
+            Op.ADD: self.exec_add,
+            Op.SUB: self.exec_sub,
+            Op.MUL: self.exec_mul,
+            Op.DIV: self.exec_div
+        }
+
+        self.memory = [0]*memory_size
+        self.ip = 0
+        self.sp = 1
+
+    def load_program(self, program):
+        self.sp = self.program_length = len(program)
+        self.memory[0:self.sp] = program
+
+    def execute(self):
+        self.ip = 0
+        self.running = True
+        while self.running and self.ip < self.program_length:
+            # Load instruction
+            instruction = Op(self.memory[self.ip])
+            n_immediates = Vm.IMMEDIATES[instruction]
+            immediates = self.memory[self.ip+1:self.ip+1+n_immediates]
+
+            # Precalculate instruction pointer
+            next_ip = self.ip + 1 + n_immediates
+            if next_ip > self.program_length:
+                raise Exception("Instruction pointer out of bounds!")
+
+            # Execute instruction
+            if len(immediates) > 0:
+                self.dispatcher[instruction](immediates)
+            else:
+                self.dispatcher[instruction]()
+
+            # Advance instruction pointer
+            if self.running:
+                self.ip = next_ip
+
+    def push_to_stack(self, value):
+        self.memory[self.sp] = value
+        self.sp += 1
+        if self.sp > len(self.memory):
+            raise Exception("Memory overflow!")
+
+    def pop_from_stack(self):
+        self.sp -= 1
+        if self.sp < self.program_length:
+            raise Exception("Memory underflow!")
+        return self.memory[self.sp]
+
+    def exec_halt(self):
+        self.running = False
+
+    def exec_push(self, immediates):
+        self.push_to_stack(immediates[0])
+
+    def exec_print(self):
+        print(self.memory[self.sp-1])
+
+    def exec_add(self):
+        r = self.pop_from_stack()
+        l = self.pop_from_stack()
+        self.push_to_stack(l+r)
+
+    def exec_sub(self):
+        r = self.pop_from_stack()
+        l = self.pop_from_stack()
+        self.push_to_stack(l-r)
+
+    def exec_mul(self):
+        r = self.pop_from_stack()
+        l = self.pop_from_stack()
+        self.push_to_stack(l*r)
+
+    def exec_div(self):
+        r = self.pop_from_stack()
+        l = self.pop_from_stack()
+        self.push_to_stack(l/r)
+        
 
 def test_evaluator():
-    eval_expr = lambda s: evaluator(parse(tokenize(s)))
+    eval_expr = lambda s: Evaluator.evaluate(Parser.parse(Lexer.tokenize(s)))
 
     # Basic literals
     assert eval_expr("1") == 1
@@ -274,6 +402,7 @@ def test_evaluator():
     assert eval_expr("4 ** 0.5") == 2.0
     assert eval_expr("5 ** 1") == 5
     assert eval_expr("9 ** 0") == 1
+    assert eval_expr("2**3**2") == 512
 
     # Power with precedence over multiplication/addition
     assert eval_expr("2 + 3 ** 2") == 11
@@ -293,7 +422,6 @@ def test_evaluator():
     assert eval_expr("(1 + 2)(2 ** 2)(1 + 1)") == 24
 
     # Edge and invalid cases
-    assert eval_expr("0 ** 0") == 1
     assert eval_expr("2 ** -1") == 0.5
     assert eval_expr("4 ** -0.5") == 0.5
     # Unary plus
@@ -331,19 +459,69 @@ def test_evaluator():
     assert eval_expr("---1") == -1
     assert eval_expr("----1") == 1
     assert eval_expr("+-+2") == -2
+def test_vm_execution():
+    import io
+    from contextlib import redirect_stdout
+    def eval_vm(expr: str) -> float:
+        tokens = Lexer.tokenize(expr)
+        ast = Parser.parse(tokens)
+        bytecode = Compiler().compile(ast)
+        vm = Vm()
+        vm.load_program(bytecode)
+
+        f = io.StringIO()
+        with redirect_stdout(f):
+            vm.execute()
+        output = f.getvalue().strip()
+        return float(output) if '.' in output else int(output)
+
+    # Same tests as in test_evaluator, adapted for VM output
+    assert eval_vm("1") == 1
+    assert eval_vm("42") == 42
+    assert eval_vm("3.5") == 3.5
+    assert eval_vm("1 + 2") == 3
+    assert eval_vm("5 - 2") == 3
+    assert eval_vm("4 * 3") == 12
+    assert eval_vm("8 / 2") == 4
+    assert eval_vm("2 + 3 * 4") == 14 
+    assert eval_vm("(2 + 3) * 4") == 20
+    assert eval_vm("2 * (3 + 4)") == 14
+    assert eval_vm("(1 + 2)(3 + 4)") == 21
+    #assert eval_vm("2 ** 3") == 8
+    #assert eval_vm("4 ** 0.5") == 2.0
+    #assert eval_vm("2 ** 3 * 4") == 32
+    #assert eval_vm("(2 + 1)(3 ** 2)") == 27
+    assert eval_vm("-2 * 3") == -6
+    assert eval_vm("2 * -3") == -6
+    assert eval_vm("-2 * -3") == 6
+    assert eval_vm("4 / -2") == -2
+    assert eval_vm("-4 / -2") == 2
+    assert eval_vm("--1") == 1
+    assert eval_vm("+-+2") == -2
+    #assert eval_vm("-2 ** 2") == -4
+    #assert eval_vm("(-2) ** 2") == 4
+    #assert eval_vm("-(2 ** 2)") == -4
+    #assert eval_vm("+2 ** 3") == 8
 
 def main():
     test_evaluator()
+    test_vm_execution()
+    print("All tests passed")
     while True:
         try:
             raw = input(">> ")
             if raw:
                 iter = PeakIter(raw)
-                tokens = tokenize(raw)
+                tokens = Lexer.tokenize(raw)
             else:
                 exit()
-            ast = parse(tokens)
-            print(evaluator(ast))
+            ast = Parser.parse(tokens)
+            bytecode = Compiler().compile(ast)
+            vm = Vm()
+            vm.load_program(bytecode)
+            #vm.print_program()
+            vm.execute()
+            #output = Evaluator.evaluate(ast)
         except SyntaxError as err:
             print("SyntaxError: ", err)
             continue
@@ -353,4 +531,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
